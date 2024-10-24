8550f802169d9dac0b2be67a37b271c695754e57
Shafiqa Abdat <137359248+shafiqa15@users.noreply.github.com>
2024-10-24 12:30:38 +0300

    make the page responsive, make the board container scrollable,add delete button

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import "../Board/Board.css";
 
-const Card = ({ task, index, handleDeleteTask }) => {
+const Card = ({ task, index, handleDeleteTask, handletaskdone }) => {
   return (
     <div className={`task ${task.isDeleted ? "deleted" : ""}`}>
-      <p>
+      <p style={{ textDecoration: task.isDeleted ? "line-through" : "none" }}>
         Task {index + 1}: {task.text}
       </p>
-      <button id="delete_button" onClick={() => handleDeleteTask(index)}>
-        Delete <FontAwesomeIcon icon={faTrashAlt} />
-      </button>
+      <div className="button-container">
+        <button id="delete_button" onClick={() => handleDeleteTask(index)}>
+          Delete <FontAwesomeIcon icon={faTrashAlt} />
+        </button>
+        <button id="delete_button2" onClick={() => handletaskdone(index)}>
+          Done <FontAwesomeIcon icon={faCheck} />
+        </button>
+      </div>
     </div>
   );
 };
